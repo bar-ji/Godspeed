1115de06c9fcd9a58a5b9f12f54586378ab326b5
Barji <66711017+BarjiYT@users.noreply.github.com>
2021-09-26 16:45:32 +0100

    Finished Pause menu functionality

--- Godspeed/Assets/Scripts/UI/PauseMenu.cs
+++ Godspeed/Assets/Scripts/UI/PauseMenu.cs
@@ -9,17 +9,23 @@ using UnityEngine;
 
 public enum PauseState
 {
-    Video, Audio, Debug
+    Video, Audio, Debug, None
 }
 
 public class PauseMenu : MonoBehaviour
 {
     public static PauseMenu instance;
-    
+
     //Nested Pause State
     [SerializeField] private List<PauseButton> buttons;
+
     [SerializeField] private MovableImageData sidebarData;
     [SerializeField] private Transform sidebar;
+
+    [SerializeField] private GameObject video;
+    [SerializeField] private new GameObject audio;
+    [SerializeField] private GameObject debug;
+
     private PauseState currentState;
 
     //Menu State
@@ -31,27 +37,57 @@ public class PauseMenu : MonoBehaviour
     {
         if (instance == null) instance = this;
         else Destroy(this);
-        
+
         OnPause += OnPaused;
         OnUnpause += OnUnpaused;
-        
+
         //Initialise the state to be unpaused
         OnUnpause();
-        OnChangeState(PauseState.Video);
     }
 
     public void OnChangeState(PauseState state)
     {
+        SetContentState(currentState, false);
         currentState = state;
-        for(int i = 0; i < buttons.Count; i++)
+        SetContentState(currentState, true);
+        
+        SetButtonState(state);
+    }
+
+    void SetButtonState(PauseState state)
+    {
+        for (int i = 0; i < buttons.Count; i++)
         {
-            if (i == (int)state)
+            if (i == (int) state)
             {
                 buttons[i].OnSelected();
             }
             else
                 buttons[i].OnDeselected();
         }
+    }
+
+    void SetContentState(PauseState state, bool activeState)
+    {
+        switch (state)
+        {
+            case PauseState.Video:
+                video.SetActive(activeState);
+                break;
+            case PauseState.Audio:
+                audio.SetActive(activeState);
+                break;
+            case PauseState.Debug:
+                debug.SetActive(activeState);
+                break;
+        }
+    }
+
+    void DisableContent()
+    {
+        video.SetActive(false);
+        audio.SetActive(false);
+        debug.SetActive(false);
     }
 
     void OnPaused()
@@ -68,6 +104,8 @@ public class PauseMenu : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         isPaused = false;
         sidebar.DOLocalMove(sidebarData.startPos, sidebarData.time).SetEase(sidebarData.easeType);
+        DisableContent();
+        SetButtonState(PauseState.None);
     }
 
     public void SetIsPaused(bool value) => isPaused = value;
